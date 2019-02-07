959b1b101e9ebdb1faddcf9107e51d33cf398a8a
will.schick <schicwp@gmail.com>
2019-02-07 14:04:33 -0700

    added SetOwner action

---
+++ src/main/java/org/schicwp/dinky/workflow/hooks/SetOwner.java
@@ -0,0 +1,40 @@
+package org.schicwp.dinky.workflow.hooks;
+
+import org.schicwp.dinky.auth.AuthService;
+import org.schicwp.dinky.model.ContentMap;
+import org.schicwp.dinky.workflow.ActionHook;
+import org.schicwp.dinky.workflow.ActionHookFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by will.schick on 1/5/19.
+ */
+@Component
+public class SetOwner implements ActionHookFactory {
+
+
+    @Autowired
+    AuthService authService;
+
+    @Override
+    public String getName() {
+        return "SetOwner";
+    }
+
+    @Override
+    public ActionHook createActionHook(ContentMap config) {
+
+        String globalUser = config.getAsOrDefault("user",authService.getCurrentUser().getUsername());
+
+
+        return (content, actionConfig) -> {
+
+            String user = actionConfig.getAsOrDefault("user",globalUser);
+
+            content.setOwner(user);
+
+        };
+    }
+
+}
